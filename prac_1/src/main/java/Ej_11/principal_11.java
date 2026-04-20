271eeed3fc36549e0aba920f470a80a42b4d0b55
NoeliaPorta <portanoeliabri@gmail.com>
2026-04-19 21:01:24 -0300

    Ejercicio10: Pasar de Binario a Decimal.  Finalizado.

--- prac_1/src/main/java/Ej_11/principal_11.java
+++ prac_1/src/main/java/Ej_11/principal_11.java
@@ -7,8 +7,43 @@ sistema numérico binario, el dígito más a la derecha tiene un valor posiciona
 dígito a la izquierda tiene un valor posicional de 2, luego 4, luego 8, etcétera. El equivalente
 decimal del número binario 1101 es 1 * 1 + 0 * 2 + 1 * 4 + 1 * 8 = 13].
  */
+import java.util.Scanner;
+
 public class principal_11 {
     public static void main(String[] args) {
+        Scanner lector = new Scanner(System.in);
+        String binario ;
 
+        do{
+            System.out.print("Ingrese un valor binario: ");
+            binario = lector.next();
+            // expresión regular (Regex)
+            // ^[01]+$ significa: "Desde el inicio al fin, solo acepta caracteres 0 o 1"
+        }while(!binario.matches("^[01]+$"));
+
+        System.out.println("Binario ingresado " + binario);
+
+        long numBinario = Long.parseLong(binario);
+        long decimal = 0;
+        int potencia = 0;
+
+        while (numBinario != 0) {
+            //Agarro el último dígito (el de la derecha)
+            long ultimoDigito = numBinario % 10;
+
+            // si es un 1, sumo la potencia de 2 correspondiente
+            if (ultimoDigito == 1) {
+                decimal += Math.pow(2, potencia);
+            }
+
+            // "Eliminamo" el ultimo digito del número
+            numBinario = numBinario / 10;
+
+            // aumento la potencia
+            potencia++;
+        }
+
+        System.out.println("El equivalente decimal es: " + decimal);
+        lector.close();
     }
 }
